5946629fadbdf3068f58effba6c5bbd3e1f2bc4e
Ronan Scotet <102806074+domov44@users.noreply.github.com>
2024-05-10 17:39:55 +0200

    [ADD] ssr home page articles loading

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,33 +1,14 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { Grid, Paper, Typography, Chip } from '@mui/material';
 import Container from '@mui/material/Container';
 import CustomButton from '@/components/CustomButton';
 
-export default function Dashboard() {
-  const [articles, setArticles] = useState([]);
-
-  useEffect(() => {
-    try {
-      fetch('http://localhost:8000')
-        .then(response => {
-          if (!response.ok) {
-            throw new Error('Network response was not ok');
-          }
-          return response.json();
-        })
-        .then(data => {
-          setArticles(data);
-        });
-    } catch (error) {
-      console.error('Error fetching articles:', error);
-    }
-  }, []);
-
+export default function Dashboard({ articles }) {
   return (
     <Container component="main" maxWidth="lg">
       <Grid container spacing={3}>
         {articles.map(article => (
-          <Grid item xs={12} key={article.id}> {/* Utilisation d'une seule colonne */}
+          <Grid item xs={12} key={article.id}>
             <Paper elevation={3} style={{ padding: 16 }}>
               <div style={{ marginTop: 8 }}>
                 {article.user.name && <Chip label={article.user.name} variant="outlined" size="small" style={{ marginRight: 4 }} />}
@@ -44,3 +25,16 @@ export default function Dashboard() {
   );
 }
 
+export async function getServerSideProps() {
+  try {
+    const response = await fetch('http://localhost:8000');
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    const data = await response.json();
+    return { props: { articles: data } };
+  } catch (error) {
+    console.error('Error fetching articles:', error);
+    return { props: { articles: [] } };
+  }
+}
